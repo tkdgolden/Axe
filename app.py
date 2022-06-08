94bebfbfd11b5d2c3308e1865fa75572c23030f4
tkdgolden <olivia.golden.w@gmail.com>
2022-06-07 20:36:08 -0500

    basic website framework

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    if not session.get("name"):
+        return render_template("index.html")
+    return render_template("judgehome.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
