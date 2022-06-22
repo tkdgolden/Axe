7f4560ba9859672a00fab655f46a11c55c4a3863
tkdgolden <olivia.golden.w@gmail.com>
2022-06-22 15:15:25 -0500

    added beginning of postgres

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+
+app.debug = False
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://aitqeogodbgmfm:03f15e7e90089ce3fd408a2bceb1fede8257af98d0a7a24819d20305a47b39c0@ec2-3-218-171-44.compute-1.amazonaws.com:5432/dc4jn4js71cvkk'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
