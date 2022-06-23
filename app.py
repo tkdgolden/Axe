c3fced3b02702bb8deda7aab6294fcf813e62eff
tkdgolden <olivia.golden.w@gmail.com>
2022-06-22 21:02:26 -0500

    an escape character in the url?

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ Session(app)
 pg_url = os.environ.get("PG_URL")
 
 app.debug = False
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql:\aitqeogodbgmfm:03f15e7e90089ce3fd408a2bceb1fede8257af98d0a7a24819d20305a47b39c0@ec2-3-218-171-44.compute-1.amazonaws.com:5432\dc4jn4js71cvkk'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql:\\aitqeogodbgmfm:03f15e7e90089ce3fd408a2bceb1fede8257af98d0a7a24819d20305a47b39c0@ec2-3-218-171-44.compute-1.amazonaws.com:5432\dc4jn4js71cvkk'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
